d872c20e2e824d793c9bde10f5cecbe17b18c36d
Kevin <839458828@qq.com>
2017-07-31 15:42:34 +0800

    title(需求):添加网络请求demo

--- app/src/main/java/com/zice/xz/HistogramActivity.java
+++ app/src/main/java/com/zice/xz/HistogramActivity.java
@@ -2,12 +2,47 @@ package com.zice.xz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.zice.xz.network.TestNet;
+
+import java.util.HashMap;
 
 public class HistogramActivity extends AppCompatActivity {
+
+    private Button btnTestPost;
+    private TestNet testPost;
+    private HashMap<String, String> params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_histogram);
+        initView();
+        setListener();
+        testPost = new TestNet();
+    }
+
+    private void setListener() {
+        btnTestPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                params = new HashMap<>();
+                params.put("money", "12.00");
+                params.put("name", "Test");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        testPost.requestGet(params);
+                        testPost.requestPost(params);
+                    }
+                }).start();
+            }
+        });
+    }
+
+    private void initView() {
+        btnTestPost = (Button) findViewById(R.id.btn_test_post);
     }
 }
---
+++ app/src/main/java/com/zice/xz/network/TestNet.java
@@ -0,0 +1,166 @@
+package com.zice.xz.network;
+
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+/**
+ * Copyright (c) 2017,xxxxxx All rights reserved.
+ * author：Z.kai
+ * date：2017/7/31
+ * description：
+ */
+
+public class TestNet {
+
+    private static final String TAG = "TestNet";
+
+    public void requestGet(HashMap<String, String> paramsMap) {
+        try {
+            String baseUrl = "http://172.31.14.77:8000/index/index?";
+            StringBuilder tempParams = new StringBuilder();
+            int pos = 0;
+            for (String key : paramsMap.keySet()) {
+                if (pos > 0) {
+                    tempParams.append("&");
+                }
+                tempParams.append(String.format("%s=%s", key, URLEncoder.encode(paramsMap.get(key), "utf-8")));
+                pos++;
+            }
+            String requestUrl = baseUrl + tempParams.toString();
+            // 新建一个URL对象
+            URL url = new URL(requestUrl);
+            // 打开一个HttpURLConnection连接
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            // 设置连接主机超时时间
+            urlConn.setConnectTimeout(5 * 1000);
+            //设置从主机读取数据超时
+            urlConn.setReadTimeout(5 * 1000);
+            // 设置是否使用缓存  默认是true
+            urlConn.setUseCaches(true);
+            // 设置为Post请求
+            urlConn.setRequestMethod("GET");
+            //urlConn设置请求头信息
+            //设置请求中的媒体类型信息。
+            urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            //设置客户端与服务连接类型
+            urlConn.addRequestProperty("Connection", "Keep-Alive");
+            // 开始连接
+            urlConn.connect();
+            // 判断请求是否成功
+            if (urlConn.getResponseCode() == 200) {
+                // 获取返回的数据
+                String result = streamToString(urlConn.getInputStream());
+                Log.i(TAG, "Get方式请求成功，result--->" + result);
+            } else {
+                Log.i(TAG, "Get方式请求失败");
+            }
+            // 关闭连接
+            urlConn.disconnect();
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public void requestPost(HashMap<String, String> paramsMap) {
+        try {
+            String baseUrl = "http://172.31.14.77:8000/index/index";
+            //合成参数
+            StringBuilder tempParams = new StringBuilder();
+            int pos = 0;
+            for (String key : paramsMap.keySet()) {
+                if (pos > 0) {
+                    tempParams.append("&");
+                }
+                tempParams.append(String.format("%s=%s", key, URLEncoder.encode(paramsMap.get(key), "utf-8")));
+                pos++;
+            }
+            String params = tempParams.toString();
+            Log.i(TAG, "kai ---- requestPost params ----> " + params);
+            // 请求的参数转换为byte数组
+            byte[] postData = params.getBytes();
+            // 新建一个URL对象
+            URL url = new URL(baseUrl);
+            // 打开一个HttpURLConnection连接
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            // 设置连接超时时间
+            urlConn.setConnectTimeout(5 * 1000);
+            //设置从主机读取数据超时
+            urlConn.setReadTimeout(5 * 1000);
+            // Post请求必须设置允许输出 默认false
+            urlConn.setDoOutput(true);
+            //设置请求允许输入 默认是true
+            urlConn.setDoInput(true);
+            // Post请求不能使用缓存
+            urlConn.setUseCaches(false);
+            // 设置为Post请求
+            urlConn.setRequestMethod("POST");
+            //设置本次连接是否自动处理重定向
+            urlConn.setInstanceFollowRedirects(true);
+            //我们请求的数据  
+            String data = "money=" + URLEncoder.encode("22", "UTF-8") + "&name=" + URLEncoder.encode("TEST", "UTF-8");
+            byte[] bytes = data.getBytes();
+            //设置请求体的类型是文本类型
+            urlConn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            //设置请求体的长度
+            urlConn.setRequestProperty("Content-Length", String.valueOf(bytes.length));
+            // 开始连接
+            urlConn.connect();
+            // 发送请求参数
+            OutputStream outputStream = urlConn.getOutputStream();
+            outputStream.write(bytes);
+            outputStream.flush();
+            outputStream.close();
+
+//            DataOutputStream dos = new DataOutputStream(urlConn.getOutputStream());
+//            dos.write(data.getBytes());
+//            dos.flush();
+//            dos.close();
+            Log.i(TAG, "kai ---- requestPost urlConn ----> " + urlConn.getResponseCode());
+            // 判断请求是否成功
+            if (urlConn.getResponseCode() == 200) {
+                // 获取返回的数据
+                String result = streamToString(urlConn.getInputStream());
+                Log.i(TAG, "Post方式请求成功，result--->" + result);
+            } else {
+                Log.i(TAG, "Post方式请求失败");
+            }
+            // 关闭连接
+            urlConn.disconnect();
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    /**
+     * 将输入流转换成字符串
+     *
+     * @param is 从网络获取的输入流
+     * @return
+     */
+    private String streamToString(InputStream is) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            baos.close();
+            is.close();
+            byte[] byteArray = baos.toByteArray();
+            return new String(byteArray);
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+            return null;
+        }
+    }
+
+
+}
